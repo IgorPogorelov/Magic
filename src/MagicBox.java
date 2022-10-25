b62a423fcccf64a87d5a54f857357c9a94d10689
Igor Pogorelov <pogoreelov@yandex.ru>
2022-10-25 22:54:08 +0500

    Доработка

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -2,41 +2,49 @@ import java.util.Random;
 
 public class MagicBox<T> {
     private int numbers;
-    T[] items;
+    private T[] items;
 
-    public MagicBox (int numbers) {
+    public T[] getItems() {
+        return items;
+    }
+
+    public void setItems(T[] items) {
+        this.items = items;
+    }
+
+    public MagicBox(int numbers) {
         this.numbers = numbers;
         this.items = (T[]) new Object[numbers];
     }
 
     public boolean add(T item) {
-        boolean x = false;
+        boolean itemNullOrNot = false;
         for (int i = 0; i < items.length; i++) {
             if (items[i] == null) {
                 items[i] = item;
-                x = true;
+                itemNullOrNot = true;
                 break;
             }
         }
-        return x;
-    } 
-    
-    public T pick() throws RuntimeException{
-        boolean y = false;
+        return itemNullOrNot;
+    }
+
+    public T pick() throws RuntimeException {
+        boolean itemNullOrNot = false;
         int count = 0;
         for (T item : items) {
             if (item == null) {
-                y = true;
+                itemNullOrNot = true;
                 break;
             }
             count++;
         }
-            if (y) {
-                throw new RuntimeException("Коробка ещё не полна, осталось заполнить " + (numbers - count) + " ячеек");
-            } else {
-                Random random = new Random();
-                int randomInt = random.nextInt(numbers);
-                return items[randomInt];
-            }
+        if (itemNullOrNot) {
+            throw new RuntimeException("Коробка ещё не полна, осталось заполнить " + (numbers - count) + " ячеек");
+        } else {
+            Random random = new Random();
+            int randomInt = random.nextInt(numbers);
+            return items[randomInt];
+        }
     }
 }
